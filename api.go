14795774505a5d4d3f52cce60ca88835c6a7604d
Alkorin <github@alkorin.fr>
2016-06-14 22:18:46 +0200

    Move API's code into api.go

--- api.go
+++ api.go
@@ -1,9 +1,41 @@
 package main
 
-import ()
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"golang.org/x/net/websocket"
+)
 
 type ApiVisualizer struct {
 	Name       string                        `json:"name"`
 	ID         uint64                        `json:"id"`
 	Properties map[string]VisualizerProperty `json:"properties"`
+}
+
+func (l *LedManager) StartApi() {
+	http.Handle("/buffer", websocket.Handler(func(ws *websocket.Conn) {
+		for range time.Tick(100 * time.Millisecond) {
+			j, _ := json.Marshal(l.buffer)
+			_, err := ws.Write(j)
+			if err != nil {
+				break
+			}
+		}
+	}))
+	http.HandleFunc("/api/visualizer", func(w http.ResponseWriter, r *http.Request) {
+		m := make([]ApiVisualizer, 0, len(l.visualizers))
+		for _, v := range l.visualizers {
+			m = append(m, ApiVisualizer{
+				Name:       v.Name(),
+				ID:         v.ID(),
+				Properties: GetVisualizerProperties(v),
+			})
+		}
+		j, _ := json.Marshal(m)
+		w.Write(j)
+	})
+	http.Handle("/", http.FileServer(http.Dir("./web")))
+	http.ListenAndServe(":8080", nil)
 }
--- manager.go
+++ manager.go
@@ -1,12 +1,7 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
-	"net/http"
-	"time"
-
-	"golang.org/x/net/websocket"
 )
 
 type LedManager struct {
@@ -68,27 +63,5 @@ func (l *LedManager) Start() {
 	}
 
 	// Start HTTP server
-	http.Handle("/buffer", websocket.Handler(func(ws *websocket.Conn) {
-		for range time.Tick(100 * time.Millisecond) {
-			j, _ := json.Marshal(l.buffer)
-			_, err := ws.Write(j)
-			if err != nil {
-				break
-			}
-		}
-	}))
-	http.HandleFunc("/api/visualizer", func(w http.ResponseWriter, r *http.Request) {
-		m := make([]ApiVisualizer, 0, len(l.visualizers))
-		for _, v := range l.visualizers {
-			m = append(m, ApiVisualizer{
-				Name:       v.Name(),
-				ID:         v.ID(),
-				Properties: GetVisualizerProperties(v),
-			})
-		}
-		j, _ := json.Marshal(m)
-		w.Write(j)
-	})
-	http.Handle("/", http.FileServer(http.Dir("./web")))
-	go http.ListenAndServe(":8080", nil)
+	go l.StartApi()
 }
