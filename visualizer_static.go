b237c214d4047f2d05aadac873af99a75b96639c
Alkorin <github@alkorin.fr>
2016-06-14 22:09:01 +0200

    Expose Color property of StaticColorVisualizer

--- visualizer_static.go
+++ visualizer_static.go
@@ -6,21 +6,21 @@ type StaticColorVisualizer struct {
 	BaseVisualizer
 
 	length int
-	color  Led
+	Color  Led `property:"rw"`
 }
 
 func NewStaticColorVisualizer(length int, color Led) *StaticColorVisualizer {
 	return &StaticColorVisualizer{
 		BaseVisualizer: *NewBaseVisualizer("Static"),
 		length:         length,
-		color:          color,
+		Color:          color,
 	}
 }
 
 func (v *StaticColorVisualizer) Start() {
 	d := make([]Led, v.length)
 	for i := range d {
-		d[i] = v.color
+		d[i] = v.Color
 	}
 	v.SendData(d)
 }
