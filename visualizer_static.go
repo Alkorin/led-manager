e9bcd061f4cb5682fa024050e4ee6e1ab34a86b8
Alkorin <github@alkorin.fr>
2016-06-15 23:08:43 +0200

    Split StaticColorVisualizer's Color into RGBW properties

--- visualizer_static.go
+++ visualizer_static.go
@@ -6,21 +6,28 @@ type StaticColorVisualizer struct {
 	BaseVisualizer
 
 	length int
-	Color  Led `property:"rw"`
+	Red    float64 `property:"rw"`
+	Green  float64 `property:"rw"`
+	Blue   float64 `property:"rw"`
+	White  float64 `property:"rw"`
 }
 
 func NewStaticColorVisualizer(length int, color Led) *StaticColorVisualizer {
 	return &StaticColorVisualizer{
-		BaseVisualizer: *NewBaseVisualizer("Static"),
+		BaseVisualizer: *NewBaseVisualizer("StaticColor"),
 		length:         length,
-		Color:          color,
+		Red:            color.Red,
+		Green:          color.Green,
+		Blue:           color.Blue,
+		White:          color.White,
 	}
 }
 
 func (v *StaticColorVisualizer) Start() {
 	d := make([]Led, v.length)
+	color := Led{v.Red, v.Green, v.Blue, v.White}
 	for i := range d {
-		d[i] = v.Color
+		d[i] = color
 	}
 	v.SendData(d)
 }
