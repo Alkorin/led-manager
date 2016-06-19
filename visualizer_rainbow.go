81d42b012bfa4f8b323e91709a1e850a8e9ddcd0
Alkorin <github@alkorin.fr>
2016-06-19 23:10:13 +0200

    Add Luminosity as property of visualizer_rainbow

--- visualizer_rainbow.go
+++ visualizer_rainbow.go
@@ -9,25 +9,27 @@ type RainbowVisualizer struct {
 
 	length int
 
-	Speed float64 `property:"rw"`
+	Speed      float64 `property:"rw"`
+	Luminosity float64 `property:"rw"`
 }
 
 func NewRainbowVisualizer(length int) *RainbowVisualizer {
 	return &RainbowVisualizer{
 		BaseVisualizer: *NewBaseVisualizer("Rainbow"),
 		length:         length,
-		Speed:          0.01,
+		Speed:          0.5,
+		Luminosity:     1.0,
 	}
 }
 
 func (v *RainbowVisualizer) Start() {
 	d := make([]Led, v.length)
 	j := 0.0
-	for range time.Tick(100 * time.Millisecond) {
-		j += v.Speed
+	for range time.Tick(10 * time.Millisecond) {
+		j += v.Speed / 100
 		for i := 0; i < v.length; i++ {
 			r, g, b := hueToRGB(j + float64(i)/128.0)
-			d[i] = Led{r, g, b, 0}
+			d[i] = Led{r * v.Luminosity, g * v.Luminosity, b * v.Luminosity, 0}
 		}
 		v.SendData(d)
 	}
