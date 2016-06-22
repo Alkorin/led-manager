02524b876ac1049b6455bc4f5834be58955878d0
Alkorin <github@alkorin.fr>
2016-06-22 22:27:49 +0200

    Handle reverse strip order for renderer_esp8266

--- renderer_esp8266.go
+++ renderer_esp8266.go
@@ -11,9 +11,10 @@ type Esp8266Renderer struct {
 	buffer   []byte
 	conn     *net.UDPConn
 	destAddr *net.UDPAddr
+	reverse  bool
 }
 
-func NewEsp8266Renderer(size int, dest string) *Esp8266Renderer {
+func NewEsp8266Renderer(size int, dest string, reverse bool) *Esp8266Renderer {
 	// Check dest validity
 	destAddr, err := net.ResolveUDPAddr("udp", dest)
 	if err != nil {
@@ -29,17 +30,28 @@ func NewEsp8266Renderer(size int, dest string) *Esp8266Renderer {
 		buffer:         make([]byte, 4*size),
 		conn:           conn,
 		destAddr:       destAddr,
+		reverse:        reverse,
 	}
 }
 
 func (r *Esp8266Renderer) Start() {
 	for range time.Tick(10 * time.Millisecond) {
 		data := r.GetData()
-		for i, color := range data {
-			r.buffer[4*i+0] = byte(255 * color.Green)
-			r.buffer[4*i+1] = byte(255 * color.Red)
-			r.buffer[4*i+2] = byte(255 * color.Blue)
-			r.buffer[4*i+3] = byte(255 * color.White)
+		if r.reverse {
+			l := len(r.buffer)
+			for i, color := range data {
+				r.buffer[l-(4*(i+1))+0] = byte(255 * color.Green)
+				r.buffer[l-(4*(i+1))+1] = byte(255 * color.Red)
+				r.buffer[l-(4*(i+1))+2] = byte(255 * color.Blue)
+				r.buffer[l-(4*(i+1))+3] = byte(255 * color.White)
+			}
+		} else {
+			for i, color := range data {
+				r.buffer[4*i+0] = byte(255 * color.Green)
+				r.buffer[4*i+1] = byte(255 * color.Red)
+				r.buffer[4*i+2] = byte(255 * color.Blue)
+				r.buffer[4*i+3] = byte(255 * color.White)
+			}
 		}
 		r.conn.WriteToUDP(r.buffer, r.destAddr)
 	}
