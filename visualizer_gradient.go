8005f365ce8453ec46af1bcb2feb3f236878040b
Alkorin <github@alkorin.fr>
2016-06-30 23:19:39 +0200

    Add Interpolation method choice on visualizer_gradient

--- visualizer_gradient.go
+++ visualizer_gradient.go
@@ -1,13 +1,18 @@
 package main
 
-import ()
+import (
+	"math"
+
+	"github.com/bthomson/go-color"
+)
 
 type GradientVisualizer struct {
 	BaseVisualizer
 
-	length     int
-	StartColor ColorRGB `property:"rw"`
-	EndColor   ColorRGB `property:"rw"`
+	length        int
+	StartColor    ColorRGB `property:"rw"`
+	EndColor      ColorRGB `property:"rw"`
+	Interpolation string   `property:"rw" enum:"RGB,HSV+,HSV-"`
 }
 
 func NewGradientVisualizer(length int, start, end ColorRGB) *GradientVisualizer {
@@ -16,6 +21,7 @@ func NewGradientVisualizer(length int, start, end ColorRGB) *GradientVisualizer 
 		length:         length,
 		StartColor:     start,
 		EndColor:       end,
+		Interpolation:  "RGB",
 	}
 }
 
@@ -30,15 +36,42 @@ func (v *GradientVisualizer) OnPropertyChanged(string) {
 func (v *GradientVisualizer) SendColors() {
 	d := make([]Led, v.length)
 	length := float64(v.length)
-	dr := (v.EndColor.Red - v.StartColor.Red) / (length - 1)
-	dg := (v.EndColor.Green - v.StartColor.Green) / (length - 1)
-	db := (v.EndColor.Blue - v.StartColor.Blue) / (length - 1)
-	led := Led{v.StartColor.Red, v.StartColor.Green, v.StartColor.Blue, 0}
-	for i := range d {
-		d[i] = led
-		led.Red += dr
-		led.Green += dg
-		led.Blue += db
+	switch v.Interpolation {
+	case "RGB":
+		dr := (v.EndColor.Red - v.StartColor.Red) / (length - 1)
+		dg := (v.EndColor.Green - v.StartColor.Green) / (length - 1)
+		db := (v.EndColor.Blue - v.StartColor.Blue) / (length - 1)
+		led := Led{v.StartColor.Red, v.StartColor.Green, v.StartColor.Blue, 0}
+		for i := range d {
+			d[i] = led
+			led.Red += dr
+			led.Green += dg
+			led.Blue += db
+		}
+	case "HSV+", "HSV-":
+		startHSL := color.RGB{v.StartColor.Red, v.StartColor.Green, v.StartColor.Blue}.ToHSL()
+		endHSL := color.RGB{v.EndColor.Red, v.EndColor.Green, v.EndColor.Blue}.ToHSL()
+
+		if v.Interpolation == "HSV+" && endHSL.H < startHSL.H {
+			endHSL.H += 1
+		} else if v.Interpolation == "HSV-" && endHSL.H > startHSL.H {
+			endHSL.H -= 1
+		}
+
+		dh := (endHSL.H - startHSL.H) / (length - 1)
+		ds := (endHSL.S - startHSL.S) / (length - 1)
+		dl := (endHSL.L - startHSL.L) / (length - 1)
+		for i := range d {
+			rgb := startHSL.ToRGB()
+
+			d[i].Red = rgb.R
+			d[i].Green = rgb.G
+			d[i].Blue = rgb.B
+
+			_, startHSL.H = math.Modf(startHSL.H + dh)
+			startHSL.S += ds
+			startHSL.L += dl
+		}
 	}
 	v.SendData(d)
 }
